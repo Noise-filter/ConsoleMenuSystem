a6eedcddad4532e97f7a94a76293c6770d105a82
Pontus Fransson <pontus.fransson92@gmail.com>
2015-03-16 23:12:18 +0100

    Added comments for creation of all items

--- Console menu system/Code/Console menu system/Test.h
+++ Console menu system/Code/Console menu system/Test.h
@@ -16,12 +16,15 @@ public:
 
 	void Init()
 	{
+		//Username
 		menu.AddMenuItem(new MenuSystem::TextLabel(MenuSystem::Utility::Pos(80, 0), MenuSystem::Utility::Text("Username:", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(0, 0)));
 		menu.AddMenuItem(new MenuSystem::InputField<Test*>(this, Test::asd, MenuSystem::Utility::Pos(80, 1), MenuSystem::Utility::Text("", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(20, 1), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		
+		//Password
 		menu.AddMenuItem(new MenuSystem::TextLabel(MenuSystem::Utility::Pos(80, 3), MenuSystem::Utility::Text("Password:", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(0, 0)));
 		menu.AddMenuItem(new MenuSystem::InputFieldPassword<Test*>(this, Test::asd, MenuSystem::Utility::Pos(80, 4), MenuSystem::Utility::Text("", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(20, 1), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 
+		//9 buttons
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(5, 5), MenuSystem::Utility::Text("Test button", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(11,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(20, 10), MenuSystem::Utility::Text("Test button2", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(20, 5), MenuSystem::Utility::Text("Test button3", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
@@ -31,8 +34,11 @@ public:
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(35, 5), MenuSystem::Utility::Text("Test button7", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(35, 10), MenuSystem::Utility::Text("Test button8", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		menu.AddMenuItem(new MenuSystem::Button<Test*>(this, Test::asd, MenuSystem::Utility::Pos(35, 15), MenuSystem::Utility::Text("Test button9", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
+		
+		//Checkbox
 		menu.AddMenuItem(new MenuSystem::Checkbox<Test*>(this, Test::asd, MenuSystem::Utility::Pos(0, 0), MenuSystem::Utility::Text("Checkbox", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos(12,0), MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_Green, MenuSystem::Utility::COLOR_Red)));
 		
+		//Checkbox list
 		std::vector<std::string> checkboxes;
 		checkboxes.push_back("Checkbox 1");
 		checkboxes.push_back("Checkbox 2");
@@ -51,6 +57,7 @@ public:
 		asd->AddItem("Checkbox 5", 2);
 		menu.AddMenuItem(asd);
 
+		//Labels
 		selected = new MenuSystem::TextLabel(MenuSystem::Utility::Pos(20, 20), MenuSystem::Utility::Text("", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos());
 		lastDeselected = new MenuSystem::TextLabel(MenuSystem::Utility::Pos(20, 21), MenuSystem::Utility::Text("", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos());
 		lastPressed = new MenuSystem::TextLabel(MenuSystem::Utility::Pos(20, 22), MenuSystem::Utility::Text("", MenuSystem::Utility::TextColor(MenuSystem::Utility::COLOR_White, MenuSystem::Utility::COLOR_Black)), MenuSystem::Utility::Pos());
